509cd3d785b214048338601202386df798c77c7d
beakHyeonMyeong <81284265+whitewise95@users.noreply.github.com>
2022-05-20 20:50:59 +0900

    Only API exists

---
+++ example/JPA_CRUD_ examples/Only API exists/src/main/java/com/sparta/sparta_spring/SpartaSpringApplication.java
@@ -0,0 +1,45 @@
+package com.sparta.sparta_spring;
+
+import com.sparta.sparta_spring.domain.*;
+import com.sparta.sparta_spring.service.CourseService;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.List;
+
+@EnableJpaAuditing // 수정일자 생성일자 자동으로 등록 할 수 있게 해준다.
+@SpringBootApplication
+public class SpartaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaSpringApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(CourseRepository courseRepository, CourseService courseService) {
+        return (args) -> {
+            courseRepository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
+
+            System.out.println("데이터 인쇄");
+            List<Course> courseList = courseRepository.findAll();
+            for (int i = 0; i < courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
+            }
+
+            CourseRequestDto requestDto = new CourseRequestDto("웹개발의 봄, Spring", "임민영");
+            courseService.update(1L, requestDto);
+            courseList = courseRepository.findAll();
+            for (int i = 0; i < courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
+            }
+        };
+    }
+}
